243b032b4981141de4279d17394d400c22afe54f
Segunbakare <167519754+Segunbakare@users.noreply.github.com>
2024-04-20 01:34:29 +0100

    Add files via upload

---
+++ SQLQuery view1.sql
@@ -0,0 +1,9 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingPeopleVaccinated]
+  FROM [portfoilioproject].[dbo].[percenatpeoplevaccinated]
+
+  select * from percenatpeoplevaccinated
